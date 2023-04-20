0f21ef791b86f157c97ab29fb34476bb328e1266
cicca16 <cicca1611@gmail.com>
2023-04-19 22:20:35 -0300

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+const NavBar = () => {
+
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Noticias</li>
+                <li>Sucursales</li>
+                <li>Produtos</li>
+            </ul>
+        </nav>
+    )
+    
+
+}
+
+export default NavBar;
